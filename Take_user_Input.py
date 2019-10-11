431404598719a0ed0edf7d2894eefbc8a64572cf
Manoj Parihar <33658925+ManojParihar@users.noreply.github.com>
2019-10-11 10:35:13 +0530

    Update Take_user_Input.py

--- Take_user_Input.py
+++ Take_user_Input.py
@@ -1,7 +1,7 @@
 # age = input("Enter your age: ")         // In python user input is in string form. This line will not work because of str input
 age = int(input("Enter your age: "))      # To treat string as integer we have to put int before input.
 if age >= 18 :                               # If and else statement (* colon at last :)
-    print('Adult')                        # See output
+    print('Adult')                        
 else :
     print('Child')   
 
